10e3e394dbe5cccbba2d17f65b0ec08441c92f7d
hoang92-gtvt <hoang92.gtvt@gmail.com>
2021-09-08 09:54:48 +0700

    run MySQl done

--- src/main/java/com/cmc/testConfig/config/DataSourceConfig.java
+++ src/main/java/com/cmc/testConfig/config/DataSourceConfig.java
@@ -1,5 +1,7 @@
 package com.cmc.testConfig.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +13,13 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @Configuration
 public class DataSourceConfig {
 
-
     @Bean(name="dataSoure")
-    @Primary
+//    @Primary
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
@@ -28,17 +30,42 @@ public class DataSourceConfig {
         return dataSource;
     }
 
-
-//    @Bean
-//    public SqlSessionFactory sqlSessionFactory() throws Exception  {
-//        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-//        factoryBean.setDataSource(dataSource());
-//        factoryBean.setTransactionFactory(new ManagedTransactionFactory());
-//        return factoryBean.getObject();
+//    @Bean(name="dataSoure")
+////    @Primary
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//        dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:database2");
+//        dataSource.setUsername("hr");
+//        dataSource.setPassword("h123");
+//        System.out.println("tao duoc datasoure");
+//        return dataSource;
 //    }
 
+//        @Bean
+//    public DataSource dataSource() throws SQLException {
+//            HikariConfig hikariConfig = new HikariConfig();
+//            hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//            hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:database2");
+//            hikariConfig.setUsername("hr");
+//            hikariConfig.setPassword("h123");
+//
+//            HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+//
+//        return dataSource;
+//    }
+
+
+
+
+    @Bean(name="transactionManager")
+    public DataSourceTransactionManager transactionManager() throws SQLException {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+
     @Bean(name="sqlSessionFactoryBean")
-    public SqlSessionFactoryBean sqlSessionFactoryBean() throws IOException {
+    public SqlSessionFactoryBean sqlSessionFactoryBean() throws IOException, SQLException {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource());
 
@@ -65,28 +92,12 @@ public class DataSourceConfig {
         return mapperScannerConfigurer;
     }
 
-    @Bean(name="transactionManager")
-    public DataSourceTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
 
 
 
 
 
-//    @Bean
-//    public DataSource dataSource() throws SQLException {
-//        PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
-//        dataSource.setUser("books");
-//        dataSource.setPassword("books");
-//        dataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
-//        dataSource.setURL("jdbc:oracle:thin:@//localhost:11521/ORCLPDB1");
-//        dataSource.setFastConnectionFailoverEnabled(true);
-//        dataSource.setInitialPoolSize(5);
-//        dataSource.setMinPoolSize(5);
-//        dataSource.setMaxPoolSize(10);
-//        return dataSource;
-//    }
+
 
 
 }
--- src/main/java/com/cmc/testConfig/mapper/IUserMapper.java
+++ src/main/java/com/cmc/testConfig/mapper/IUserMapper.java
@@ -8,18 +8,18 @@ import java.util.List;
 @Mapper
 public interface IUserMapper  {
 
-    @Insert("insert into users(name, password) values(#{name}, #{password})")
+    @Insert("insert into usersdemo(name, password) values(#{name}, #{password})")
     public void add(User user);
 
-    @Update("update users set name=#{name}, password=#{password} where id=#{id}")
+    @Update("update usersdemo set name=#{name}, password=#{password} where id=#{id}")
     public void update(User user);
 
-    @Select("select * from users where id=#{id}")
+    @Select("select * from usersdemo where id=#{id}")
     public User findById(long id);
 
-    @Select("select * from users")
+    @Select("select * from usersdemo")
     public List<User> findAll();
 
-    @Delete("delete from users where id=#{id}")
+    @Delete("delete from usersdemo where id=#{id}")
     public void delete(long id);
 }
